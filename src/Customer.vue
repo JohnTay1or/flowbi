e08d1c111773c5660962fe60fb32e8c14eda5c96
John Taylor <JohnTay1or@me.com>
2017-06-03 21:46:39 +1000

    Added Customer vue 2

---
+++ src/Customer.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="customer">
+    <el-button @click.native="getData">Data Button</el-button>
+    <form>
+      <span class="error" v-show="!message">
+        You must enter a message
+      </span>
+      <textarea v-model="message"></textarea>
+      <button type="submit" v-show="message">Send Message</button>
+    </form>
+    <pre>{{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    getData () {
+      console.log('In get data')
+    }
+    /*startHacking () {
+      /*var Connection = require('tedious').Connection;
+      var Request = require('tedious').Request;
+      // Create connection to database
+      var config = {
+        userName: 'your_username', // update me
+        password: 'your_password', // update me
+        server: 'your_server.database.windows.net', // update me
+        options: {
+          database: 'your_database' //update me
+        }
+      }
+      var connection = new Connection(config);
+
+      // Attempt to connect and execute queries if connection goes through
+      connection.on('connect', function(err) {
+        if (err) {
+          console.log(err)
+        }
+        else{
+          //queryDatabase()
+          console.log('Success');
+        }
+});
+    }*/
+  }
+}
+</script>
+
+<style>
+  .gap {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+</style>
