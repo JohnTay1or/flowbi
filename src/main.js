b802f9de6dbce94d91c92ca70a8fdb57d13797be
John Taylor <JohnTay1or@me.com>
2017-06-07 11:41:13 +1000

    Switched to production API fix

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ Vue.use(ElementUI)
 Vue.use(VueRouter)
 Vue.use(VueResource)
 //Vue.http.options.root = 'http://127.0.0.1:1337/api/'
-Vue.http.options.root = 'https://flowbi.herokuapp.com'
+Vue.http.options.root = 'https://flowbi.herokuapp.com/api'
 
 Vue.component('app-menu', Menu)
 
