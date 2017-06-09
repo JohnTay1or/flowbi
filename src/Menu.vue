78dc2760d78a38d1139358fbad393fdc7f3c874c
John Taylor <JohnTay1or@me.com>
2017-06-09 17:36:59 +1000

    Demo embeding

--- src/Menu.vue
+++ src/Menu.vue
@@ -9,7 +9,8 @@
     </el-menu-item>
     <el-submenu index="2">
       <template slot="title">Visualization</template>
-      <el-menu-item index="2-1"><a href="http://127.0.0.1:1337/">Map</a></el-menu-item>
+      <!--<el-menu-item index="2-1"><a href="http://127.0.0.1:1337/">Map</a></el-menu-item>-->
+      <el-menu-item index="2-1"><a href="http://flowbi.azurewebsites.net/">Map</a></el-menu-item>
       <!--<el-menu-item index="2-1"><router-link to="/visual">Map</router-link></el-menu-item>
       <el-menu-item index="2-2"><router-link to="/visual">Trend</router-link></el-menu-item>-->
 
